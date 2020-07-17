1b84d2558274f0f81217c31849dc3275fe369f02
dmblack <dev_node@zendpoint.pantsoptional.net>
2020-07-17 20:23:33 +1000

    Minor tweaks to design and style

--- src/ISMControl.js
+++ src/ISMControl.js
@@ -7,27 +7,34 @@ function ISMControl(props) {
     <div className="list-group-item list-group-item-action flex-column align-tems-start control">
       <ScrollableAnchor id={props.control.Identifier}>
         <div className="d-flex w-100 justify-content-between control-identifier">
-	        <h4 className="mb-1">
+	        <h1 className="mb-1">
 	          {props.control.Identifier}
-	        </h4>
-	        <small>
-            {props.control.OFFICIAL === "Yes" ? " OFFICIAL " : " "}
-            {props.control.PROTECTED === "Yes" ? "PROTECTED " : ""}
-            {props.control.SECRET === "Yes" ? "SECRET " : ""}
-            {props.control.TOP_SECRET === "Yes" ? "TOP-SECRET " : ""}
+	        </h1>
+	        <small className="control-applicability">
+	          [
+              {props.control.OFFICIAL === "Yes" ? " OFFICIAL " : " "}
+              {props.control.PROTECTED === "Yes" ? "PROTECTED " : ""}
+              {props.control.SECRET === "Yes" ? "SECRET " : ""}
+              {props.control.TOP_SECRET === "Yes" ? "TOP_SECRET " : ""}
+            ]
 	        </small>
 	      </div>
 	    </ScrollableAnchor>
-	    <div className="d-flex w-100 justify-content-between control-applicability">
-	      <h5 className="mb-1">
+	    <h4 className="control-guideline">
+	      {props.control.Guideline.split('Guidelines for ')[1]}
+	    </h4>
+	    <h6 className="control-topic">
+	      {props.control.Topic}
+	    </h6>
+	    <p className="mb-1 control-description">
+	      {props.control.Description}
+	    </p>
+	    <div className="d-flex w-100 justify-content-between">
+	      <p className="mb-1"></p>
+	      <small className="control-revision-updated">
 	        Rev:{props.control.Revision} - {props.control.Updated}
-	      </h5>
-	      <small>
-	        {props.control.Guideline}
 	      </small>
 	    </div>
-	    <p className="mb-1">{props.control.Description}</p>
-	    <small>{props.control.Section} - {props.control.Topic}</small>
     </div>
   );
 }

943cc5b91083eeeb8c7237538bf9e048f2ba0dfb
dmblack <14871038+dmblack@users.noreply.github.com>
2021-04-15 18:46:35 +1000

    Updated for passing TESTS.

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,31 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('Renders search form.', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Description/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('Ensures App Renders...', () => {
+  describe('Ensures Search...', () => {
+    test('Is rendered.', () => {
+      const app = render(<App />);
+      const searchTitle = app.container.querySelector('#search');
+      expect(searchTitle).toBeInTheDocument();
+    });
+    
+    test('has a description query.', () => {
+      const app = render(<App />);
+      const descriptionElement = app.container.querySelector('#description');
+      expect(descriptionElement).toBeInTheDocument();
+    });
+    
+    test('has a guideline query.', () => {
+      const app = render(<App />);
+      const searchElement = app.container.querySelector('#guideline');
+      expect(searchElement).toBeInTheDocument();
+    });
+    
+    test('has an identifier query.', () => {
+      const app = render(<App />);
+      const searchElement = app.container.querySelector('#identifier');
+      expect(searchElement).toBeInTheDocument();
+    });
+  })
+})

8c943e09957363005eeefedda00a44422633fcda
dmblack <14871038+dmblack@users.noreply.github.com>
2021-07-15 22:02:36 +1000

    Updated App.css Added collapse/expand styles.
    Updated App.js
    Exposed faChevronDown/Up for the collapse search section.
    Added state to handle.
    Added collapse handler.
    Added behavior to search and chevrons onclick.
    Wrapped search in div to hide/show.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 import {
   useLocation
 } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
+
 
 const useQuery = () => {
   return new URLSearchParams(useLocation().search);
@@ -45,7 +48,8 @@ const App = () => {
     controlsTagged: urlControlsTagged,
     descriptionFilter: '',
     guidelineFilter: '',
-    identifierFilter: urlControls
+    identifierFilter: urlControls,
+    searchCollapse: false
   });
 
   useEffect(() => {
@@ -78,6 +82,7 @@ const App = () => {
   const handleDescriptionChange = e => setInterrogate({...interrogate, descriptionFilter: e.target.value});
 	const handleGuidelineChange = e => setInterrogate({...interrogate, guidelineFilter: e.target.value});
 	const handleIdentifierChange = e => setInterrogate({...interrogate, identifierFilter: e.target.value});
+  const handleSearchCollapse = e => setInterrogate({...interrogate, searchCollapse: !interrogate.searchCollapse})
 
   const handleTagControl = identifier => {
     const newTaggedControls = interrogate.controlsTagged.includes(identifier)
@@ -95,56 +100,69 @@ const App = () => {
 
   return (
     <div className="App container">
-      <div className="modal-header">
-        <h4 className="title" id="search">Search</h4>
-      </div>
-      <div className="form-group row filters">
-        <label
-          htmlFor="description"
-          className="col-sm-2 col-form-label">
-          Description
-        </label>
-        <DebounceInput
-          minLength={2}
-          debounceTimeout={350}
-          id="description"
-          value={interrogate.descriptionFilter}
-          type="text"
-          onChange={handleDescriptionChange}
-          className="form-control col-sm-10"
+      <div className="flex-row modal-header" onClick={handleSearchCollapse} >
+        <h4 className={`d-flex w-100 justify-content-between collapse-content ${interrogate.searchCollapse ? 'collapsed' : 'expanded'}`} id="search">Search</h4>
+        <FontAwesomeIcon
+          className={`chevron collapse-content ${interrogate.searchCollapse ? 'collapsed' : 'expanded'}`}
+          icon={faChevronUp}
+          size="1x"
+          />
+        <FontAwesomeIcon
+          className={`chevron collapse-content ${interrogate.searchCollapse ? 'expanded' : 'collapsed'}`}
+          icon={faChevronDown}
+          size="1x"
         />
       </div>
-      <div className="form-group row">
-        <label
-          htmlFor="guideline"
-          className="col-sm-2 col-form-label">
-          Guideline
-        </label>
-        <select
-          name="guideline"
-          id="guideline"
-          onChange={handleGuidelineChange}
-          className="form-control col-sm-10">
-          <option value=""></option>
-          {guidelineOptions}
-        </select>
-      </div>
-      <div className="form-group row">
-        <label
-          htmlFor="identifier"
-          className="col-sm-2 col-form-label">
-          Identifier
-        </label>
-        <DebounceInput
-          minLength={1}
-          debounceTimeout={350}
-          id="identifier"
-          value={interrogate.identifierFilter}
-          type="text"
-          onChange={handleIdentifierChange}
-          className="form-control col-sm-10"
-          data-tip="Separate multiple with ','"
-        />
+      <div className={`collapse-content ${interrogate.searchCollapse ? 'collapsed' : 'expanded'}`}
+        aria-expanded={interrogate.searchCollapse}>
+        <div className="form-group row filters">
+          <label
+            htmlFor="description"
+            className="col-sm-2 col-form-label">
+            Description
+          </label>
+          <DebounceInput
+            minLength={2}
+            debounceTimeout={350}
+            id="description"
+            value={interrogate.descriptionFilter}
+            type="text"
+            onChange={handleDescriptionChange}
+            className="form-control col-sm-10"
+          />
+        </div>
+        <div className="form-group row">
+          <label
+            htmlFor="guideline"
+            className="col-sm-2 col-form-label">
+            Guideline
+          </label>
+          <select
+            name="guideline"
+            id="guideline"
+            onChange={handleGuidelineChange}
+            className="form-control col-sm-10">
+            <option value=""></option>
+            {guidelineOptions}
+          </select>
+        </div>
+        <div className="form-group row">
+          <label
+            htmlFor="identifier"
+            className="col-sm-2 col-form-label">
+            Identifier
+          </label>
+          <DebounceInput
+            minLength={1}
+            debounceTimeout={350}
+            id="identifier"
+            value={interrogate.identifierFilter}
+            type="text"
+            onChange={handleIdentifierChange}
+            className="form-control col-sm-10"
+            data-tip="Separate multiple with ','"
+          />
+        </div>
       </div>
       <div className="modal-header">
         <h4 className="control-counter">Controls ({interrogate.controlList.length})</h4>
@@ -153,7 +171,7 @@ const App = () => {
       <div className="list-group">
         {
         interrogate.controlList
-          .map((control) => <ISMControl control={control} key={control.Identifier} tag={() => { handleTagControl(control.Identifier)}} tagged={interrogate.controlsTagged.includes(control.Identifier)} />)
+          .map((control) => <ISMControl control={control} key={control.Identifier} tag={() => { handleTagControl(control.Identifier)}} tagged={interrogate.controlsTagged.includes(control.Identifier)} test={() => { console.log('Test')}} />)
         }
       </div>
     </div>

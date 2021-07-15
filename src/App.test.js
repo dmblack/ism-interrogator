2b6d2ec6c65cbb09696bdd170a152db249bc6cbe
dmblack <14871038+dmblack@users.noreply.github.com>
2021-07-15 22:09:20 +1000

    Updated App.test.js Fixes tests failing after adding react-router. Wrapp App tests in suitable router.

--- src/App.test.js
+++ src/App.test.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
-
+import { MemoryRouter } from 'react-router-dom';
 
 describe('Ensures App Renders...', () => {
   describe('Ensures Search...', () => {
     test('Is rendered.', () => {
-      const app = render(<App />);
+      const app = render( <MemoryRouter><App /></MemoryRouter>);
       const searchTitle = app.container.querySelector('#search');
       expect(searchTitle).toBeInTheDocument();
     });
     
     test('has a description query.', () => {
-      const app = render(<App />);
+      const app = render( <MemoryRouter><App /></MemoryRouter>);
       const descriptionElement = app.container.querySelector('#description');
       expect(descriptionElement).toBeInTheDocument();
     });
     
     test('has a guideline query.', () => {
-      const app = render(<App />);
+      const app = render( <MemoryRouter><App /></MemoryRouter>);
       const searchElement = app.container.querySelector('#guideline');
       expect(searchElement).toBeInTheDocument();
     });
     
     test('has an identifier query.', () => {
-      const app = render(<App />);
+      const app = render( <MemoryRouter><App /></MemoryRouter>);
       const searchElement = app.container.querySelector('#identifier');
       expect(searchElement).toBeInTheDocument();
     });
